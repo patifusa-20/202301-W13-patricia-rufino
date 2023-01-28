421f32678b525f0927fe3ee88ae24e03b9de37c8
patridifusa <p.rufino.a@gmail.com>
2023-01-28 10:26:23 +0000

    Fix bug in custom hook

--- src/hooks/use.products.tsx
+++ src/hooks/use.products.tsx
@@ -5,14 +5,10 @@ import { UseProductsStructure } from "../types/use.products.type";
 
 export function useProducts(): UseProductsStructure {
     const items = categoriesData;
-    const initialCategoryState = categoriesData[0];
+    const initialCategoryState = items[0];
     const [item, setItem] = useState(initialCategoryState);
 
     const handleFilter = (item: GenericStructure) => {
-        items.map((category) => {
-            category.id !== item.id ? (category.isSelected = false) : "";
-        });
-
         setItem({ ...item });
     };
     return { item, items, handleFilter };
