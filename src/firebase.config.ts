5047e6e6c492629785af6d992296fda14228cd8e
patridifusa <p.rufino.a@gmail.com>
2023-01-28 23:32:07 +0000

    Install and config Firebase

---
+++ src/firebase.config.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+    apiKey: "AIzaSyATLWK1n7_cjMpnfwdgPGKSfnXSYM8rqI4",
+    authDomain: "digital-menu-app-5cd01.firebaseapp.com",
+    projectId: "digital-menu-app-5cd01",
+    storageBucket: "digital-menu-app-5cd01.appspot.com",
+    messagingSenderId: "555789150324",
+    appId: "1:555789150324:web:20fca5e38322ac6d910921",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(firebaseApp);
