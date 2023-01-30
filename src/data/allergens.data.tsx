48213d224ad5b9a5d471276abb98bcc61588c181
patridifusa <p.rufino.a@gmail.com>
2023-01-30 13:35:10 +0000

    Remove no allergen data

--- src/data/allergens.data.tsx
+++ src/data/allergens.data.tsx
@@ -1,18 +1,9 @@
 import { GenericModel } from "../model/generic.model";
 
-const noAllergen = new GenericModel(
-    "Sin alérgenos",
-    "./assets/icons/icon-no-allergen.svg"
-);
 const glutenAllergen = new GenericModel(
     "gluten",
     "./assets/icons/icon-gluten.svg"
 );
 const nutAllergen = new GenericModel("nut", "./assets/icons/icon-nut.svg");
 const milkAllergen = new GenericModel("milk", "./assets/icons/icon-milk.svg");
-export const allergensData = [
-    noAllergen,
-    glutenAllergen,
-    nutAllergen,
-    milkAllergen,
-];
+export const allergensData = [glutenAllergen, nutAllergen, milkAllergen];
