5ab470c048726b357073f0278b689693c07ff64d
patridifusa <p.rufino.a@gmail.com>
2023-02-02 23:43:30 +0000

    Update function to get product and change test

--- src/pages/edit/edit.page.test.tsx
+++ src/pages/edit/edit.page.test.tsx
@@ -1,7 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { Edit } from "../../components/product.edit/edit";
+import { ProductsContext } from "../../context/products.context";
 import { GenericModel } from "../../model/generic.model";
 import { ProductModel } from "../../model/product.model";
+import { ProductsContextStructure } from "../../types/products.context.type";
 import EditPage from "./edit.page";
 jest.mock("../../components/product.edit/edit");
 
@@ -15,6 +18,13 @@ const mockProduct = new ProductModel(
     mockAllergens,
     false
 );
+mockProduct.id = "16";
+
+const products = [mockProduct];
+
+const mockContext = {
+    products,
+} as unknown as ProductsContextStructure;
 
 describe("Given EditPage component", () => {
     describe("When it has been render", () => {
@@ -22,13 +32,29 @@ describe("Given EditPage component", () => {
             (Edit as jest.Mock).mockImplementation(() => {
                 return <p>Mock Edit</p>;
             });
+            render(
+                <MemoryRouter initialEntries={["/edit/16"]} initialIndex={0}>
+                    <ProductsContext.Provider value={mockContext}>
+                        <Routes>
+                            <Route
+                                path={"/edit/:id"}
+                                element={<EditPage />}
+                            ></Route>
+                        </Routes>
+                    </ProductsContext.Provider>
+                </MemoryRouter>
+            );
+            screen.debug();
         });
         test("Then the heading should be in the screen", () => {
-            render(<EditPage />);
             const element = screen.getByRole("heading", {
                 name: "Editar producto",
             });
             expect(element).toBeInTheDocument();
+        });
+        test("Then the text in edit component should be in the screen", () => {
+            const elementEdit = screen.getByText(/Mock Edit/i);
+            expect(elementEdit).toBeInTheDocument();
         });
     });
 });
--- src/pages/edit/edit.page.tsx
+++ src/pages/edit/edit.page.tsx
@@ -2,19 +2,20 @@ import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Edit } from "../../components/product.edit/edit";
 import { ProductsContext } from "../../context/products.context";
+import { ProductStructure } from "../../types/product.type";
 
 export default function EditPage() {
     const { products } = useContext(ProductsContext);
     const pathName = useParams();
 
+    const productToEdit = products.find(
+        (item) => item.id === pathName.id
+    ) as ProductStructure;
+
     return (
         <>
             <h1>Editar producto</h1>
-            {products.map((item) => {
-                if (item.id === pathName.id) {
-                    return <Edit key={item.id} product={item}></Edit>;
-                }
-            })}
+            <Edit key={productToEdit.id} product={productToEdit}></Edit>
         </>
     );
 }
