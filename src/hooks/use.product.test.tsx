86c4d135fd83a41cb2c981baaea3ab3f831f3b96
patridifusa <p.rufino.a@gmail.com>
2023-02-11 11:14:08 +0000

    Add mock file to cusotm hook test

--- src/hooks/use.product.test.tsx
+++ src/hooks/use.product.test.tsx
@@ -1,13 +1,21 @@
 import { act, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
-import { GenericModel } from "../model/generic.model";
-import { ProductModel } from "../model/product.model";
 import { MenuRepo } from "../repository/menus.repo";
 import { ProductRepo } from "../repository/products.repo";
 import { useProduct } from "./use.product";
 import { getAuth } from "firebase/auth";
 import { UserRepo } from "../repository/users.repo";
+import {
+    mockProduct1,
+    mockAddProduct,
+    mockUpdateProduct,
+    mockProducts,
+    mockMenu1,
+    mockMenus,
+    mockRepoResponse,
+    mockRepoResponseCase3,
+} from "./mock";
 jest.mock("firebase/auth");
 
 jest.mock("../repository/products.repo");
@@ -22,88 +30,9 @@ MenuRepo.prototype.load = jest.fn();
 jest.mock("../repository/users.repo");
 UserRepo.prototype.load = jest.fn();
 
-const mockCategory = "Test category";
-const mockAllergen = [new GenericModel("Test allergen", "Test allergen icon")];
-
 describe(`Given useProduct (custom hook)
             render with a virtual component with case 1`, () => {
-    const mockProduct = new ProductModel(
-        "Test name 1",
-        "Test image 1",
-        "Test price 1",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockProduct.id = "0030";
-    const mockProduct1 = new ProductModel(
-        "Test name 1a",
-        "Test image 1a",
-        "Test price 1a",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockProduct1.id = "0035";
-
-    const mockNewObj = { name: "0040" };
-
-    const mockAddProduct = new ProductModel(
-        "Test name added",
-        "Test image added",
-        "Test price added",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockAddProduct.id = "0040";
-
-    const mockUpdateProduct = new ProductModel(
-        "Test name updated",
-        "Test image updated",
-        "Test price updated",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockUpdateProduct.id = "0050";
-
-    const mockProducts = [mockProduct, mockProduct1, mockUpdateProduct];
-
-    const mockMenu1 = { id: "0508", name: "NameMenu1" };
-    const mockMenus = [mockMenu1];
-
-    const mockUser = {
-        id: "0158",
-        name: "Name Logged User",
-        token: "Token Logged User",
-        menu: { id: "0508" },
-    };
-    const mockUser2 = {
-        id: "0325",
-        name: "Name Logged User2",
-        token: "Token Logged User2",
-        menu: { id: "0509" },
-    };
-    const mockUsers = [mockUser, mockUser2];
-
-    const mockRepoResponse = () => {
-        (ProductRepo.prototype.load as jest.Mock).mockResolvedValue(
-            mockProducts
-        );
-        (ProductRepo.prototype.create as jest.Mock).mockResolvedValue(
-            mockNewObj
-        );
-        (ProductRepo.prototype.update as jest.Mock).mockResolvedValue(
-            mockUpdateProduct
-        );
-        (ProductRepo.prototype.delete as jest.Mock).mockResolvedValue(
-            mockProduct1.id
-        );
-        (MenuRepo.prototype.load as jest.Mock).mockResolvedValue(mockMenus);
-        (UserRepo.prototype.load as jest.Mock).mockResolvedValue(mockUsers);
-    };
-    let TestComponent: () => JSX.Element;
+    let TestComponentCase1: () => JSX.Element;
     let buttons: Array<HTMLElement>;
     beforeEach(async () => {
         (getAuth as jest.Mock).mockImplementation(() => {
@@ -115,7 +44,7 @@ describe(`Given useProduct (custom hook)
                 },
             };
         });
-        TestComponent = () => {
+        TestComponentCase1 = () => {
             const {
                 handleLoad,
                 handleAdd,
@@ -171,7 +100,7 @@ describe(`Given useProduct (custom hook)
         await act(() =>
             render(
                 <MemoryRouter>
-                    <TestComponent />
+                    <TestComponentCase1 />
                 </MemoryRouter>
             )
         );
@@ -228,77 +157,7 @@ describe(`Given useProduct (custom hook)
 
 describe(`Given useProduct (custom hook)
             render with a virtual component with case 2`, () => {
-    const mockProduct = new ProductModel(
-        "Test name 1",
-        "Test image 1",
-        "Test price 1",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockProduct.id = "0030";
-    const mockProduct1 = new ProductModel(
-        "Test name 1a",
-        "Test image 1a",
-        "Test price 1a",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockProduct1.id = "0035";
-
-    const mockNewObj = { name: "0040" };
-
-    const mockAddProduct = new ProductModel(
-        "Test name added",
-        "Test image added",
-        "Test price added",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockAddProduct.id = "0040";
-
-    const mockUpdateProduct = new ProductModel(
-        "Test name updated",
-        "Test image updated",
-        "Test price updated",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockUpdateProduct.id = "0050";
-
-    const mockProducts = [mockProduct, mockProduct1, mockUpdateProduct];
-
-    const mockMenu1 = { id: "0508", name: "NameMenu1" };
-    const mockMenus = [mockMenu1];
-
-    const mockUser = {
-        id: "0158",
-        name: "Name Logged User",
-        token: "Token Logged User",
-        menu: { id: "0508" },
-    };
-    const mockUser2 = {
-        id: "0325",
-        name: "Name Logged User2",
-        token: "Token Logged User2",
-        menu: { id: "0509" },
-    };
-    const mockUsers = [mockUser, mockUser2];
-
-    const mockRepoResponse = () => {
-        (ProductRepo.prototype.load as jest.Mock).mockResolvedValue(
-            mockProducts
-        );
-        (ProductRepo.prototype.create as jest.Mock).mockResolvedValue(
-            mockNewObj
-        );
-        (MenuRepo.prototype.load as jest.Mock).mockResolvedValue(mockMenus);
-        (UserRepo.prototype.load as jest.Mock).mockResolvedValue(mockUsers);
-    };
-    let TestComponent: () => JSX.Element;
+    let TestComponentCase2: () => JSX.Element;
     let buttons: Array<HTMLElement>;
     beforeEach(async () => {
         (getAuth as jest.Mock).mockImplementation(() => {
@@ -306,7 +165,7 @@ describe(`Given useProduct (custom hook)
                 currentUser: null,
             };
         });
-        TestComponent = () => {
+        TestComponentCase2 = () => {
             const { handleLoad, handleAdd } = useProduct();
             return (
                 <>
@@ -322,7 +181,7 @@ describe(`Given useProduct (custom hook)
         await act(() =>
             render(
                 <MemoryRouter>
-                    <TestComponent />
+                    <TestComponentCase2 />
                 </MemoryRouter>
             )
         );
@@ -340,81 +199,7 @@ describe(`Given useProduct (custom hook)
 
 describe(`Given useProduct (custom hook)
             render with a virtual component with case 3`, () => {
-    const mockProduct = new ProductModel(
-        "Test name 1",
-        "Test image 1",
-        "Test price 1",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockProduct.id = "0030";
-    const mockProduct1 = new ProductModel(
-        "Test name 1a",
-        "Test image 1a",
-        "Test price 1a",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockProduct1.id = "0035";
-
-    const mockNewObj = { name: "0040" };
-
-    const mockAddProduct = new ProductModel(
-        "Test name added",
-        "Test image added",
-        "Test price added",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockAddProduct.id = "0040";
-
-    const mockUpdateProduct = new ProductModel(
-        "Test name updated",
-        "Test image updated",
-        "Test price updated",
-        mockCategory,
-        mockAllergen,
-        false
-    );
-    mockUpdateProduct.id = "0050";
-
-    const mockProducts = [mockProduct, mockProduct1, mockUpdateProduct];
-
-    const mockMenu1 = {
-        id: "0508",
-        name: "NameMenu1",
-        products: [mockProduct, mockProduct1],
-    };
-    const mockMenus = [mockMenu1];
-
-    const mockUser = {
-        id: "0158",
-        name: "Name Logged User",
-        token: "Token Logged User",
-        menu: { id: "0508" },
-    };
-    const mockUser2 = {
-        id: "0325",
-        name: "Name Logged User2",
-        token: "Token Logged User2",
-        menu: { id: "0509" },
-    };
-    const mockUsers = [mockUser, mockUser2];
-
-    const mockRepoResponse = () => {
-        (ProductRepo.prototype.load as jest.Mock).mockResolvedValue(
-            mockProducts
-        );
-        (ProductRepo.prototype.create as jest.Mock).mockResolvedValue(
-            mockNewObj
-        );
-        (MenuRepo.prototype.load as jest.Mock).mockResolvedValue(mockMenus);
-        (UserRepo.prototype.load as jest.Mock).mockResolvedValue(mockUsers);
-    };
-    let TestComponent: () => JSX.Element;
+    let TestComponentCase3: () => JSX.Element;
     let buttons: Array<HTMLElement>;
     beforeEach(async () => {
         (getAuth as jest.Mock).mockImplementation(() => {
@@ -426,7 +211,7 @@ describe(`Given useProduct (custom hook)
                 },
             };
         });
-        TestComponent = () => {
+        TestComponentCase3 = () => {
             const { handleLoad, handleAdd } = useProduct();
             return (
                 <>
@@ -437,12 +222,12 @@ describe(`Given useProduct (custom hook)
                 </>
             );
         };
-        mockRepoResponse();
+        mockRepoResponseCase3();
 
         await act(() =>
             render(
                 <MemoryRouter>
-                    <TestComponent />
+                    <TestComponentCase3 />
                 </MemoryRouter>
             )
         );
