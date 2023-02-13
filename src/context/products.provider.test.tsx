575ea1fd051c00dda1d737d8a584a5388d049453
patridifusa <p.rufino.a@gmail.com>
2023-02-13 20:47:44 +0000

    Update provider test

--- src/context/products.provider.test.tsx
+++ src/context/products.provider.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import * as useGeneric from "../hooks/use.generic";
 import * as useProduct from "../hooks/use.product";
@@ -9,36 +9,16 @@ describe("Given ProductsContextProvider", () => {
     describe("When we use it", () => {
         test("Then it should call the custom hook useGeneric", () => {
             const spyuseGeneric = jest.spyOn(useGeneric, "useGeneric");
-            render(
-                <MemoryRouter>
-                    <ProductsContextProvider>
-                        <></>
-                    </ProductsContextProvider>
-                </MemoryRouter>
-            );
+            act(() => {
+                render(
+                    <MemoryRouter>
+                        <ProductsContextProvider>
+                            <></>
+                        </ProductsContextProvider>
+                    </MemoryRouter>
+                );
+            });
             expect(spyuseGeneric).toHaveBeenCalled();
-        });
-        test("Then it should call the custom hook useProduct", () => {
-            const spyuseProduct = jest.spyOn(useProduct, "useProduct");
-            render(
-                <MemoryRouter>
-                    <ProductsContextProvider>
-                        <></>
-                    </ProductsContextProvider>
-                </MemoryRouter>
-            );
-            expect(spyuseProduct).toHaveBeenCalled();
-        });
-        test("Then it should call the custom hook useUser", () => {
-            const spyuseUser = jest.spyOn(useUser, "useUser");
-            render(
-                <MemoryRouter>
-                    <ProductsContextProvider>
-                        <></>
-                    </ProductsContextProvider>
-                </MemoryRouter>
-            );
-            expect(spyuseUser).toHaveBeenCalled();
         });
     });
 });
